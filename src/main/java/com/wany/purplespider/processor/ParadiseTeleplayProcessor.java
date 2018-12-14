33d2e9f11cb2e4a9e9dca4265bf7ecef94ded12b
wanysoul <wanysoul@163.com>
2018-12-14 22:51:21 +0800

    first commit

---
+++ src/main/java/com/wany/purplespider/processor/ParadiseTeleplayProcessor.java
@@ -0,0 +1,22 @@
+package com.wany.purplespider.processor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+public class ParadiseTeleplayProcessor implements PageProcessor {
+
+    private Site site = Site.me().setRetryTimes(3).setSleepTime(1000);
+
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+
+}
